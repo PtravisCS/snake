aa08029eb2b41d9df7e7cdbdf853a5592f3b2582
electracion <electracion@gmail.com>
2020-12-29 21:52:12 -0500

    Refactored code

--- main.c
+++ main.c
@@ -80,15 +80,16 @@ int randInRange(int low, int high) {
 
 }
 
-int main(int argc, char *argv[]) {
+/* Name     : initializeSnake
+ * Purpose  : used to initialize the primary snake object used by the game
+ * Author   : Paul Travis
+ */
+struct Snake initializeSnake() {
 
-  struct Snake snake; //Instantiate an instance of the snake 
-  struct Coords pellet; //Create a coord pair to represent the pellets the snake eats
-  snake.xvel = 0; //Set the x velocity of the snake to 0.
-  snake.yvel = 0; //Set the y velocity of the snake to 0.
-  snake.size = 1; //Set the size of the snake to 0 (size of the snake is 0 based as opposed to 1 based).
-  pellet.ycoord = randInRange(3, (LINES - 2));
-  pellet.xcoord = randInRange(3, (COLS - 2));
+  struct Snake snake;
+  snake.xvel = 0;
+  snake.yvel = 0;
+  snake.size = 1;
 
   for (int i = 0; i < 20; i++) { //Set all previous coords to -1 which the game treats as an empty coordinate set.
 
@@ -100,6 +101,30 @@ int main(int argc, char *argv[]) {
   snake.coords[0].xcoord = 4; //Set the initial x coord for the snake to 4 
   snake.coords[0].ycoord = 4; //Set the initial y coord for the snake to 4
 
+  return snake;
+
+}
+
+/* Name     : initializePellet
+ * Purpose  : used to initialize the pellet the snake eats
+ * Author   : Paul Travis
+ */
+struct Coords initializePellet() {
+
+  struct Coords pellet;
+  pellet.xcoord = randInRange(3, (LINES - 2));
+  pellet.ycoord = randInRange(3, (COLS - 2));
+  
+  return pellet;
+
+}
+
+/* Name     : prepScreen
+ * Purpose  : Preps the ncurses screen environment for the game
+ * Author   : Paul Travis
+ */
+void prepScreen() {
+
   initscr(); //Initialize the ncurses screen instance
   clear();  //Clear the screen (I really wish a library wasn't necessary for this behaviour
   noecho(); //Disable user input echoing
@@ -108,107 +133,216 @@ int main(int argc, char *argv[]) {
   curs_set(0); //Hide the terminal cursor
   keypad(stdscr, TRUE); //Enables the keypad permitting use of KEY_LEFT etc.
 
-  printw("Press 'q' to quit.");
+}
+
+/* Name     : getDirection
+ * Purpose  : processes user input to determine the direction the snake should be travelling
+ * Author   : Paul Travis
+ */
+struct Snake getDirection(struct Snake snake, int ch) {
+
+  if (ch == KEY_UP) { //If the up key is pressed
+
+    snake.xvel = 0;
+    snake.yvel = -1;
+
+  } else if (ch == KEY_DOWN) { //If the down key is pressed
+
+    snake.xvel = 0;
+    snake.yvel = 1;
+
+  } else if (ch == KEY_LEFT) { //If the left key is pressed
+
+    snake.xvel = -1;
+    snake.yvel = 0;
+
+  } else if (ch == KEY_RIGHT) { //If the right key is pressed
+
+    snake.xvel = 1;
+    snake.yvel = 0;
+
+  }
+
+  return snake;
+
+}
+
+/*
+ * Name     : printBorders
+ * Purpose  : Prints the borders of the playing field
+ * Author   : Paul Travis
+ */
+void printBorders() {
+
+  for (int i = 2; i != (COLS - 1); i++) { //Print the top and bottom borders of the map.
+
+    mvprintw(2, i, "X");
+    mvprintw(LINES - 2, i, "X");
+
+  }
+
+  for (int i = 2; i != (LINES - 1); i++) { //Print the left and right borders of the map.
+
+    mvprintw(i, 2, "X");
+    mvprintw(i, COLS - 2, "X");
+
+  }
+
+}
+
+/*
+ * Name     : moveSnake
+ * Purpose  : moves the snake coordinates to it's new location
+ * Author  : Paul Travis
+ */
+struct Snake moveSnake(struct Snake snake) {
+
+  for (int i = snake.size; i > 0; i--) { //Re-arrange the snakes coords to match it's new position.
+
+    snake.coords[i].ycoord = snake.coords[i - 1].ycoord;
+    snake.coords[i].xcoord = snake.coords[i - 1].xcoord;
+
+  }
+
+  snake.coords[0].xcoord += snake.xvel; //Move the snake in the x direction associated with it's x velocity.
+  snake.coords[0].ycoord += snake.yvel; //Move the snake in the y direction associated with it's y velocity.
+
+  return snake;
+
+}
+
+/*
+ * Name     : testSelfCollision
+ * Purpose  : test if the snake is colliding with itself, if it is end the game
+ * Author   : Paul Travis
+ */
+void testSelfCollision(struct Snake snake) {
+
+  for (int i = 1; i < snake.size; i++) { //Check for snake collision
+
+    if (snake.coords[i].ycoord == snake.coords[0].ycoord && snake.coords[i].xcoord == snake.coords[0].xcoord) { //If the head of the snake collides with the tail
+
+      if (snake.xvel != 0 || snake.yvel != 0) { //And if the snake is moving
+
+        end("Game Over! Press any key to continue"); //Game Over    
+
+      }
+
+    }
+
+
+  }
+
+}
+
+/*
+ * Name     : testSelfCollision
+ * Purpose  : test if the snake is colliding with the left or top of map, if it is end the game
+ * Author   : Paul Travis
+ */
+void testLowerBoundsCollision(struct Snake snake) {
+
+  if (snake.coords[0].ycoord < 3 || snake.coords[0].xcoord < 3) { //Check if snake goes off left or top of map
+
+    end("Game Over! Press any key to continue"); //Game Over    
+
+  }
+
+}
+
+/*
+ * Name     : testSelfCollision
+ * Purpose  : test if the snake is colliding with the right or bottom of map, if it is end the game
+ * Author   : Paul Travis
+ */
+void testUpperBoundsCollision(struct Snake snake) {
+
+  if (snake.coords[0].ycoord > (LINES - 3) || snake.coords[0].xcoord > (COLS -  3)) { //Check if snake goes off right or bottom of map
+
+    end("Game Over! Press any key to continue"); //Game Over    
+
+  }
+
+}
+
+/*
+ * Name     : testSelfCollision
+ * Purpose  : test if the snake is colliding with the sides of the map or itself, if it is end the game
+ * Author   : Paul Travis
+ */
+void testCollision(struct Snake snake) {
+
+  testSelfCollision(snake);
+  testLowerBoundsCollision(snake);
+  testUpperBoundsCollision(snake);
+
+}
+
+/*
+ * Name     : movePellet
+ * Purpose  : moves the pellet the snake eats to a new randomized location within the playing field
+ * Author   : Paul Travis
+ */
+struct Coords movePellet(struct Coords pellet) {
+
+  pellet.ycoord = randInRange(3, (LINES - 3));
+  pellet.xcoord = randInRange(3, (COLS - 3));
+  printf("\a");
+
+  return pellet;
+
+}
+
+/*
+ * Name     : printSnake
+ * Purpose  : prints the snake to the screen
+ * Author   : Paul Travis
+ */
+void printSnake(struct Snake snake) {
+
+  for (int i = 0; i < snake.size; i++) { //Print the snake to the screen
+
+    mvprintw(snake.coords[i].ycoord, snake.coords[i].xcoord, "*");
+    //mvprintw(i + 2,1,"%i: %i,%i", i, snake.coords[i].ycoord, snake.coords[i].xcoord); //uncomment this line to see the snake's coordinate array.
+
+  }
+
+}
+
+int main(int argc, char *argv[]) {
+
+  struct Snake snake = initializeSnake(); //Instantiate an instance of the snake 
+  struct Coords pellet = initializePellet(); //Create a coord pair to represent the pellets the snake eats
+
+  prepScreen();
+
   refresh(); //Refresh screen to display the exit message
   
   for (int ch = getch(); ch != 'q'; ch = getch()) { //Loop until the user presses q
 
-    if (ch == KEY_UP) { //If the up key is pressed
-  
-      snake.xvel = 0;
-      snake.yvel = -1;
-
-    } else if (ch == KEY_DOWN) { //If the down key is pressed
-
-      snake.xvel = 0;
-      snake.yvel = 1;
-
-    } else if (ch == KEY_LEFT) { //If the left key is pressed
-
-      snake.xvel = -1;
-      snake.yvel = 0;
-
-    } else if (ch == KEY_RIGHT) { //If the right key is pressed
-
-      snake.xvel = 1;
-      snake.yvel = 0;
-
-    }
+    snake = getDirection(snake, ch); //process user input and modify the snakes velocity accordingly
 
     clear(); //Clear the screen.
 
     mvprintw(0, 0, "Press 'q' to quit."); //Print the message indicating how to quit the game.
 
-    for (int i = 2; i != (COLS - 1); i++) { //Print the top and bottom borders of the map.
+    printBorders(); //print the screen borders
 
-      mvprintw(2, i, "X");
-      mvprintw(LINES - 2, i, "X");
+    snake = moveSnake(snake); //move the snake around the screen based on its current velocity
 
-    }
+    testCollision(snake); //test if the snake is colliding with the edges of the screen or itself and if it is end the game
 
-    for (int i = 2; i != (LINES - 1); i++) { //Print the left and right borders of the map.
+    if (snake.coords[0].ycoord == pellet.ycoord && snake.coords[0].xcoord == pellet.xcoord) { //Test for collision with the pellet
 
-      mvprintw(i, 2, "X");
-      mvprintw(i, COLS - 2, "X");
-
-    }
-
-
-    for (int i = snake.size; i > 0; i--) { //Re-arrange the snakes coords to match it's new position.
-
-      snake.coords[i].ycoord = snake.coords[i - 1].ycoord;
-      snake.coords[i].xcoord = snake.coords[i - 1].xcoord;
+      snake.size++; //if the snake is colliding with the pellet, increase its size by one
+      pellet = movePellet(pellet); //if the snake collided with the pellet, move it to a new location
 
     }
 
-    snake.coords[0].xcoord += snake.xvel; //Move the snake in the x direction associated with it's x velocity.
-    snake.coords[0].ycoord += snake.yvel; //Move the snake in the y direction associated with it's y velocity.
+    printSnake(snake);
 
-    for (int i = 1; i < snake.size; i++) { //Check for snake collision
-
-      if (snake.coords[i].ycoord == snake.coords[0].ycoord && snake.coords[i].xcoord == snake.coords[0].xcoord) { //If the head of the snake collides with the tail
-
-        if (snake.xvel != 0 || snake.yvel != 0) { //And if the snake is moving
-
-          end("Game Over! Press any key to continue"); //Game Over    
-
-        }
-
-      }
-
-
-    }
-
-    if (snake.coords[0].ycoord < 3 || snake.coords[0].xcoord < 3) { //Check if snake goes off left or top of map
-
-      end("Game Over! Press any key to continue"); //Game Over    
-
-    }
-
-    if (snake.coords[0].ycoord > (LINES - 3) || snake.coords[0].xcoord > (COLS -  3)) { //Check if snake goes off right or bottom of map
-
-      end("Game Over! Press any key to continue"); //Game Over    
-
-    }
-
-    if (snake.coords[0].ycoord == pellet.ycoord && snake.coords[0].xcoord == pellet.xcoord) {
-
-      snake.size += 1;
-      pellet.ycoord = randInRange(3, (LINES - 3));
-      pellet.xcoord = randInRange(3, (COLS - 3));
-      printf("\a");
-
-    }
-
-    for (int i = 0; i < snake.size; i++) { //Print the snake to the screen
-
-      mvprintw(snake.coords[i].ycoord, snake.coords[i].xcoord, "*");
-      //mvprintw(i + 2,1,"%i: %i,%i", i, snake.coords[i].ycoord, snake.coords[i].xcoord); //uncomment this line to see the snake's coordinate array.
-
-    }
-
-    mvprintw(pellet.ycoord, pellet.xcoord, "@");
-    mvprintw(1,1,"x: %i, y: %i", pellet.xcoord, pellet.ycoord);
+    mvprintw(pellet.ycoord, pellet.xcoord, "@"); //Print the pellet to the screen
+    //mvprintw(1,1,"x: %i, y: %i", pellet.xcoord, pellet.ycoord); //uncommment this line to see the pellet's coordinates
 
     refresh();
 
